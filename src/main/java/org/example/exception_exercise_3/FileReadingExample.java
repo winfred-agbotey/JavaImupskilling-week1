987cd9622d93d00b7d7c0487b3206ba14b180481
winfred-agbotey <winfred.mawuli-agbotey@amalitech.com>
2024-10-26 08:17:22 +0000

    complete exception exercise 3

--- src/main/java/org/example/exception_exercise_3/FileReadingExample.java
+++ src/main/java/org/example/exception_exercise_3/FileReadingExample.java
@@ -1,2 +1,31 @@
-package org.example.exception_exercise_3;public class FileReadingExample {
+package org.example.exception_exercise_3;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class FileReadingExample {
+    public static void readFile(String filePath) {
+        BufferedReader reader = null;
+        try {
+            reader = new BufferedReader(new FileReader(filePath));
+            System.out.println("Reading from the file:");
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            System.out.println("Caught IOException: " + e.getMessage());
+        } finally {
+            try {
+                if (reader != null) {
+                    reader.close();
+                    System.out.println("File has been closed.");
+                }
+            } catch (IOException e) {
+                System.out.println("Failed to close the file: " + e.getMessage());
+            }
+        }
+
+    }
 }
--- src/main/java/org/example/exception_exercise_3/Main.java
+++ src/main/java/org/example/exception_exercise_3/Main.java
@@ -1,2 +1,10 @@
-package org.example.exception_exercise_3;public class Main {
+package org.example.exception_exercise_3;
+
+import static org.example.exception_exercise_3.FileReadingExample.readFile;
+
+public class Main {
+    public static void main(String[] args) {
+//        readFile("nonexistent.txt");
+        readFile("C://Users//WinfredMawuliAgbotey//Downloads//upskill.pdf/");
+    }
 }
