0abc5c5ebab66864575e5f76b89589a8ec20a4e1
winfred-agbotey <winfred.mawuli-agbotey@amalitech.com>
2024-10-27 20:46:04 +0000

    complete algorithms exercise 2

--- src/main/java/org/example/algorithm_exercise_2/LCS.java
+++ src/main/java/org/example/algorithm_exercise_2/LCS.java
@@ -1,2 +1,41 @@
-package org.example.algorithm_exercise_2;public class LCS {
+package org.example.algorithm_exercise_2;
+
+public class LCS {
+
+    // Method to find the length of LCS and build the LCS string
+    public static String findLCS(String s1, String s2) {
+        int m = s1.length();
+        int n = s2.length();
+
+        // Create a 2D table to store the lengths of LCS
+        int[][] dp = new int[m + 1][n + 1];
+
+        // Fill the dp table using bottom-up dynamic programming
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+
+        // Backtrack through the table to build the LCS string
+        StringBuilder lcs = new StringBuilder();
+        int i = m, j = n;
+        while (i > 0 && j > 0) {
+            if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
+                lcs.append(s1.charAt(i - 1));
+                i--;
+                j--;
+            } else if (dp[i - 1][j] > dp[i][j - 1]) {
+                i--;
+            } else {
+                j--;
+            }
+        }
+
+        return lcs.reverse().toString(); // Reverse the string to get the correct order
+    }
 }
--- src/main/java/org/example/algorithm_exercise_2/Main.java
+++ src/main/java/org/example/algorithm_exercise_2/Main.java
@@ -1,2 +1,13 @@
-package org.example.algorithm_exercise_2;public class Main {
+package org.example.algorithm_exercise_2;
+
+import static org.example.algorithm_exercise_2.LCS.findLCS;
+
+public class Main {
+    public static void main(String[] args) {
+        String s1 = "AGGTAB";
+        String s2 = "GXTXAYB";
+
+        String lcs = findLCS(s1, s2);
+        System.out.println("Longest Common Subsequence: " + lcs);
+    }
 }
